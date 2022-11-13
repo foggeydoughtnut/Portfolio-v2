1a7a832d331054f7b9fdef98abdf0e4d046424d1
foggeydoughtnut <foggeydoughtnut@gmail.com>
2022-11-13 15:51:56 -0700

    Projects page done

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,18 +1,21 @@
+import { Show } from "solid-js";
 
 const ProjectCard = (props) => {
 
   const navigateToPage = (webPage) => {
-    console.log("this was clled")
-    window.location = webPage
+    window.open(webPage);
   }
 
   return (
-    <div class="card w-1/3 bg-base-100 shadow-xl">
+    <div class="card lg:w-1/3 w-3/4 bg-base-100 shadow-xl h-auto">
+      <Show when={props.image}>
+        <figure><img src={props.image} alt={props.image} class="content-center" /></figure>
+      </Show>
       <div class="card-body">
         <h2 class="card-title">{props.title}</h2>
         <p>{props.body}</p>
         <div class="card-actions justify-end">
-          <button class="btn btn-primary" onClick={() => window.open(props.projectLink, '_blank')}>Learn More</button>
+          <button class="btn btn-primary" onClick={() => navigateToPage(props.projectLink)}>Learn More</button>
         </div>
       </div>
     </div>
