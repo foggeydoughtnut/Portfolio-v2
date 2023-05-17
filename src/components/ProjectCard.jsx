a3f28bacee76bdd59f42d52c9f2464cc6cc5ca5c
Jeff Anderson <foggeydoughtnut@gmail.com>
2023-05-17 12:08:48 -0600

    added more details on the rest api

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,10 +1,12 @@
-import { Show } from "solid-js";
+import { Show, createSignal } from "solid-js";
 
 const ProjectCard = (props) => {
 
   const navigateToPage = (webPage) => {
     window.open(webPage);
   }
+
+  const [showDetail, setShowDetail] = createSignal(false);
 
   return (
     <div class="card lg:w-1/3 w-3/4 bg-base-100 shadow-xl h-auto">
@@ -14,6 +16,14 @@ const ProjectCard = (props) => {
       <div class="card-body">
         <h2 class="card-title">{props.title}</h2>
         <p>{props.body}</p>
+        <Show when={props.details && showDetail()}>
+          <p>{props.details}</p>
+        </Show>
+        <Show when={props.details}>
+          <div class="card-actions justify-end">
+            <button class="btn btn-primary" onClick={() => setShowDetail(!showDetail())}>Show more info</button>
+          </div>
+        </Show>
         <Show when={props.projectLink}>
           <div class="card-actions justify-end">
             <button class="btn btn-primary" onClick={() => navigateToPage(props.projectLink)}>Learn More</button>
